0066a0ec6dedf148576ada7b7bed057e8de6d6c1
irvanhidayat <101778631+irvanhidayat@users.noreply.github.com>
2022-04-21 09:52:14 +0700

    Add files via upload

---
+++ Karyawan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramKaryawan_4481
+{
+    internal class Karyawan
+    {
+        // constructor default
+        public Karyawan()
+        {
+
+        }
+        // overload constructor 
+        public Karyawan(string nama, string nik, int gajibulanan)
+        {
+            NAMA = nama;
+            NIK = nik;
+            GajiBulanan = gajibulanan;
+        }
+        // properties
+        public string NAMA { get; set; }
+        public string NIK { get; set; }
+        public int GajiBulanan { get; set; }
+    }
+}
